ddf69acc55b0cce05ae75a0046aa79523da2ddb8
Krish Khimasia <khimasiakrish@gmail.com>
2023-03-28 19:53:11 +0530

    updated process_mc

--- music-generation/process_mc.py
+++ music-generation/process_mc.py
@@ -1,12 +1,22 @@
-from pychord import Chord
+import pprint
 
-print(Chord('Cmaj7'))
-print(Chord('G#').components())
-print(Chord('Eb/G').components())
-print(Chord('Csus2').components())
-print(Chord('Bbadd9').components())
+def get_chord_blocks():
+    file = open('./data/chords.txt', 'r')
+    Lines = file.readlines()
+    count=0
+    chord_blocks={}
+    for line in Lines:
+        if(line!="\n"):
+            count+=1
+            chords=line.split()
+            for i in range(len(chords)):
+                if chords[i] in chord_blocks:
+                    chord_blocks[chords[i%len(chords)]].append(chords[(i+1)%len(chords)])
+                else:
+                    chord_blocks[chords[i%len(chords)]]=[chords[(i+1)%len(chords)]]
+    return chord_blocks
 
+block=get_chord_blocks()
 
-
-
-
+pp=pprint.PrettyPrinter()
+pp.pprint(block)
